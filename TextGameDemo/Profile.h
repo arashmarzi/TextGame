fce7e56ca492ce3094c38ef96e83b3dd781edd30
Arash Marzi <arash.marzi@gmail.com>
2013-12-26 18:32:39 -0500

    updated racetype and profile headers
    updated necessary functions and implementations; requires testing

--- TextGameDemo/Profile.h
+++ TextGameDemo/Profile.h
@@ -1,108 +1,21 @@
-/*
- * Profile.h
- *
- *  Created on: Oct 28, 2013
- *      Author: Arash
- */
-
-#ifndef PROFILE_H_
-#define PROFILE_H_
 #include <iostream>
+#include "RaceType.h"
 #include "Inventory.h"
-using namespace std;
 
 class Profile {
 public:
-	enum Race {
-		HUMAN,
-		IMMORTAL,
-		ELF,
-		GOBLIN,
-		INTERNETTROLL,
-		INSECTOID,
-		MAMMAL,
-		AQUATIC,
-		AMPHIBIAN,
-		AERIAL,
-		DRAGON
-	};
-
-	/*Constructor for specific profile*/
-	inline Profile(string _name, int _level, Race _race, double _currentHealth,
-			double _maxHealth) :
-			name(_name), level(_level), race(_race), currentHealth(
-					_currentHealth), maxHealth(_maxHealth), isAlive(true) {
-
-	}
-
-
-	/*Constructor for default profile*/
-	inline Profile(string _name, int _level, Race _race) :
-			name(_name), level(_level), race(_race), maxHealth(100), currentHealth(
-					100), isAlive(true) {
-
-	}
-
-	inline virtual ~Profile(){
-	}
-
-	/*Character dies*/
-	inline void deathOfCharacter() {
-		cout << name << ", you have unfortunately died...what a pity;" << endl
-				<< "though you managed to reach level " << level << "!" << endl;
-	}
-
-	/*Check if character is alive*/
-	inline void isCharacterAlive() {
-		if (currentHealth == 0) {
-			isAlive = false;
-			deathOfCharacter();
-		}
-	}
-
-	/*Increment character current health*/
-	inline void incrementCurrentHealth(int incrHealth) {
-		if (currentHealth < maxHealth) {
-			if (currentHealth + incrHealth < maxHealth) {
-				currentHealth += incrHealth;
-			} else if (currentHealth + incrHealth >= maxHealth) {
-				currentHealth = maxHealth;
-			}
-		}
-	}
-
-	/*Decrement character current health*/
-	inline void decrementCurrentHealth(int decrHealth) {
-		if (currentHealth > 0) {
-			if (currentHealth - decrHealth > 0) {
-				currentHealth -= decrHealth;
-			} else if (currentHealth - decrHealth <= 0) {
-				currentHealth = 0;
-				deathOfCharacter();
-			}
-		}
-	}
-
-	/*Increment character max health*/
-	inline void incrementMaxHealth(int incrHealth) {
-		maxHealth += incrHealth;
-	}
-
-	/*Decrement character max health*/
-	inline void decrementMaxHealth(int decrHealth) {
-		if (maxHealth > 0) {
-			if (maxHealth - decrHealth > 0) {
-				maxHealth -= decrHealth;
-			} else if (maxHealth - decrHealth <= 0) {
-				maxHealth = 0;
-				currentHealth = 0;
-				isAlive = false;
-			}
-		}
-	}
-
-	/*Return profile's inventory*/
-	inline virtual Inventory getInventory();
+	Profile(string _name, int _level, Race _race, double _currentHealth, double _maxHealth);
+	Profile(string _name, int _level, Race _race);
+	void deathOfCharacter();
+	void isCharacterAlive();
+	void incrementCurrentHealth(int incrHealth);
+	void decrementCurrentHealth(int decrHealth);
+	void incrementMaxHealth(int incrHealth);
+	void decrementMaxHealth(int decrHealth);
+	virtual bool ItemUsable(const Item &item, int race);
+	virtual void updateStatsByItem(const Item &item, int race);
+	virtual void updateStatsByEnemy(const EnemyProfile &enemy, int race);
+	virtual Inventory getInventory();
 
 private:
 	string name;
@@ -115,48 +28,15 @@ private:
 
 class CharacterProfile: public Profile {
 public:
-	/*Specific character profile, use to create storyline characters*/
-	inline CharacterProfile(string _name, int _level, Race _race, double _currentHealth, double _maxHealth) :
-			Profile(_name, _level, _race, _currentHealth, _maxHealth), currentExperience(0), isAlive(true) {
-		maxExperience = 10.25 * level;
-		maxHealth = level * 10;
-	}
-
-	/*Default character profile, used to create protagonist*/
-	inline CharacterProfile(string _name) :
-			name(_name), level(5), currentHealth(100), maxHealth(100), currentExperience(
-					0), isAlive(true) {
-		maxExperience = 10.25 * level;
-		maxHealth = level * 10;
-	}
-
-	/*Increment character level*/
-	inline void incrementLevel() {
-		level += 1;
-	}
-
-	/*Increment character max experience*/
-	inline void incrementMaxExperience() {
-		maxExperience *= 10.25;
-	}
-
-	/*Increment character current experience*/
-	inline void incrementCurrentExperience(int incrExperience) {
-		if (currentExperience + incrExperience < maxExperience) {
-			currentExperience += incrExperience;
-		} else if (currentExperience + incrExperience >= maxExperience) {
-			currentExperience %= maxExperience;
-			incrementLevel();
-			incrementMaxExperience();
-		}
-	}
-
-	/*Return Character's inventory*/
-	// must adjust return type
-	inline CharacterInventory getInventory(){
-		return characterInventory;
-	}
-
+	CharacterProfile(string _name, int _level, Race _race, double _currentHealth, double _maxHealth);
+	CharacterProfile(string _name);
+	void incrementLevel();
+	void incrementMaxExperience();
+	void incrementCurrentExperience(int incrExperience);
+	bool ItemUsable(const Item &item, int race);
+	void updateStatsByItem(const Item &item, int race);
+	void updateStatsByEnemy(const EnemyProfile &enemy, int race);
+	CharacterInventory getInventory();
 private:
 	double currentExperience;
 	double maxExperience;
@@ -165,14 +45,142 @@ private:
 
 class EnemyProfile {
 public:
-	/*Return Enemy's inventory*/
-	// must adjust return type
-	inline EnemyInventory getInventory(){
-			return enemyInventory;
-		}
-
+	EnemyProfile(string _name, int _level, Race _race, double _currentHealth, double _maxHealth);
+	EnemyProfile(string _name);
+	bool ItemUsable(const Item &item, int race);
+	void updateStatsByItem(const Item &item, int race);
+	void updateStatsByEnemy(const EnemyProfile &enemy, int race);
+	EnemyInventory getInventory();
 private:
 	EnemyInventory enemyInventory;
 };
 
-#endif /* PROFILE_H_ */
+/*Constructor for specific profile*/
+Profile::Profile(string _name, int _level, Race _race, double _currentHealth,
+				 double _maxHealth) :
+name(_name), level(_level), race(_race), currentHealth(
+	_currentHealth), maxHealth(_maxHealth), isAlive(true) {
+
+}
+
+
+/*Constructor for default profile*/
+Profile::Profile(string _name, int _level, Race _race) :
+	name(_name), level(_level), race(_race), maxHealth(100), currentHealth(
+	100), isAlive(true) {
+
+}
+
+/*Character dies*/
+void Profile::deathOfCharacter() {
+	cout << name << ", you have unfortunately died...what a pity;" << endl
+		<< "though you managed to reach level " << level << "!" << endl;
+}
+
+/*Check if character is alive*/
+void Profile::isCharacterAlive() {
+	if (currentHealth == 0) {
+		isAlive = false;
+		deathOfCharacter();
+	}
+}
+
+/*Increment character current health*/
+void Profile::incrementCurrentHealth(int incrHealth) {
+	if (currentHealth < maxHealth) {
+		if (currentHealth + incrHealth < maxHealth) {
+			currentHealth += incrHealth;
+		} else if (currentHealth + incrHealth >= maxHealth) {
+			currentHealth = maxHealth;
+		}
+	}
+}
+
+/*Decrement character current health*/
+void Profile::decrementCurrentHealth(int decrHealth) {
+	if (currentHealth > 0) {
+		if (currentHealth - decrHealth > 0) {
+			currentHealth -= decrHealth;
+		} else if (currentHealth - decrHealth <= 0) {
+			currentHealth = 0;
+			deathOfCharacter();
+		}
+	}
+}
+
+/*Increment character max health*/
+void Profile::incrementMaxHealth(int incrHealth) {
+	maxHealth += incrHealth;
+}
+
+/*Decrement character max health*/
+void Profile::decrementMaxHealth(int decrHealth) {
+	if (maxHealth > 0) {
+		if (maxHealth - decrHealth > 0) {
+			maxHealth -= decrHealth;
+		} else if (maxHealth - decrHealth <= 0) {
+			maxHealth = 0;
+			currentHealth = 0;
+			isAlive = false;
+		}
+	}
+}
+
+/*Specific character profile, use to create storyline characters*/
+CharacterProfile::CharacterProfile(string _name, int _level, Race _race, double _currentHealth, double _maxHealth) :
+	Profile(_name, _level, _race, _currentHealth, _maxHealth), currentExperience(0), isAlive(true) {
+		maxExperience = 10.25 * level;
+		maxHealth = level * 10;
+}
+
+/*Default character profile, used to create protagonist*/
+CharacterProfile::CharacterProfile(string _name) :
+	name(_name), level(5), currentHealth(100), maxHealth(100), currentExperience(
+	0), isAlive(true) {
+		maxExperience = 10.25 * level;
+		maxHealth = level * 10;
+}
+
+/*Increment character level*/
+void CharacterProfile::incrementLevel() {
+	level += 1;
+}
+
+/*Increment character max experience*/
+void CharacterProfile::incrementMaxExperience() {
+	maxExperience *= 10.25;
+}
+
+/*Increment character current experience*/
+void CharacterProfile::incrementCurrentExperience(int incrExperience) {
+	if (currentExperience + incrExperience < maxExperience) {
+		currentExperience += incrExperience;
+	} else if (currentExperience + incrExperience >= maxExperience) {
+		currentExperience %= maxExperience;
+		incrementLevel();
+		incrementMaxExperience();
+	}
+}
+
+/*Return Character's inventory*/
+// must adjust return type
+CharacterInventory CharacterProfile::getInventory(){
+	return characterInventory;
+}
+
+/*Specific enemy profile, use to create storyline characters*/
+EnemyProfile::EnemyProfile(string _name, int _level, Race _race, double _currentHealth, double _maxHealth) :
+	Profile(_name, _level, _race, _currentHealth, _maxHealth), isAlive(true) {
+}
+
+/*Default enemy profile, used to create antagonists*/
+EnemyProfile::EnemyProfile(string _name) :
+	name(_name), level(5), currentHealth(100), maxHealth(100), isAlive(true) {
+}
+
+
+/*Return Enemy's inventory*/
+// must adjust return type
+EnemyInventory EnemyInventory::getInventory(){
+	return enemyInventory;
+}
---
+++ TextGameDemo/RaceType.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "Item.h"
+#include "Profile.h"
+#include "Inventory.h"
+using namespace std;
+
+class RaceType {
+	enum Race {
+		HUMAN,
+		IMMORTAL,
+		ELF,
+		GOBLIN,
+		INTERNETTROLL,
+		INSECTOID,
+		MAMMAL,
+		AQUATIC,
+		AERIAL,
+		DRAGON
+	};
+};
